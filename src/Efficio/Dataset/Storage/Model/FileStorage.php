d6e4e189ee86fafc0ce96783d8813724a8d0ea62
Marcos Minond <minond.marcos@gmail.com>
2013-10-20 01:57:36 -0600

    working on file storage trait

---
+++ src/Efficio/Dataset/Storage/Model/FileStorage.php
@@ -0,0 +1,166 @@
+<?php
+
+namespace Efficio\Dataset\Storage\Model;
+
+trait FileStorage
+{
+    /**
+     * storage directory
+     * @var string
+     */
+    protected static $dir;
+
+    /**
+     * saves model to session. returns model's id
+     * @return string
+     */
+    public function save()
+    {
+        $file = self::initStorageDirectory();
+
+        if (!$this->id) {
+            $this->id = unique();
+            touch($file . $this->id);
+        }
+
+        file_put_contents($file . $this->id, serialize($this));
+        return $this->id;
+    }
+
+    /**
+     * deletes a model. returns delete success
+     * @return boolean
+     */
+    public function delete()
+    {
+        return !$this->id ? true :
+            unlink(self::initStorageDirectory() . $this->id);
+    }
+
+    /**
+     * storage directory setter
+     * @param string $dir
+     */
+    public static function setDirectory($dir)
+    {
+        static::$dir = $dir;
+    }
+
+    /**
+     * @param callback $cb
+     * @return mixed[]|Collection
+     */
+    public static function all(callable $cb = null)
+    {
+        $dir = self::initStorageDirectory();
+        $ret = [];
+
+        foreach (scandir(self::initStorageDirectory()) as $file) {
+            if (is_file($file)) {
+                $model = unserialize(file_get_contents($dir . $file));
+                $ret[] = is_callable($cb) ? $cb($model) : $model;
+                unset($model);
+            }
+        }
+
+        if (!is_callable($cb)) {
+            $ret = static::getCollection($ret);
+        }
+
+        return $ret;
+    }
+
+    /**
+     * find a model using a unique identifier
+     * @param mixed $id
+     * @return Model
+     */
+    public static function find($id)
+    {
+        $dir = self::initStorageDirectory();
+        return !file_exists($dir . $id) ? null :
+            unserialize(file_get_contents($dir . $id));
+    }
+
+    /**
+     * find models using a set of criteria
+     * @param array $criteria
+     * @param callback $cb
+     * @return mixed[]|Collection
+     */
+    public static function findBy(array $criteria, callable $cb = null)
+    {
+        $dir = self::initStorageDirectory();
+        $ret = [];
+
+        foreach (scandir(self::initStorageDirectory()) as $file) {
+            if (is_file($file)) {
+                $model = unserialize(file_get_contents($dir . $file));
+                $match = false;
+
+                foreach ($criteria as $field => $value) {
+                    if ($model->{ $field } == $value) {
+                        $match = true;
+                    }
+                }
+
+                if ($match) {
+                    $ret[] = is_callable($cb) ? $cb($model) : $model;
+                }
+
+                unset($model);
+            }
+        }
+
+        if (!is_callable($cb)) {
+            $ret = static::getCollection($ret);
+        }
+
+        return $ret;
+    }
+
+    /**
+     * find a model using a set of criteria
+     * @param array $criteria
+     * @return Model
+     */
+    public static function findOneBy(array $criteria)
+    {
+        $dir = self::initStorageDirectory();
+        $ret = [];
+
+        foreach (scandir(self::initStorageDirectory()) as $file) {
+            if (is_file($file)) {
+                $model = unserialize(file_get_contents($dir . $file));
+                $match = false;
+
+                foreach ($criteria as $field => $value) {
+                    if ($model->{ $field } == $value) {
+                        return $model;
+                    }
+                }
+
+                unset($model);
+            }
+        }
+
+        return null;
+    }
+
+    /**
+     * @return string
+     */
+    protected static function initStorageDirectory()
+    {
+        $dir = static::$dir . DIRECTORY_SEPARATOR .
+            preg_replace('/\W+/g', '', get_called_class()) .
+            DIRECTORY_SEPARATOR;
+
+        if (!is_dir($dir)) {
+            mkdir($dir);
+        }
+
+        return $dir;
+    }
+}
+
